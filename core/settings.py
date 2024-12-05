25bc8ae99bce6234f1080e24422439169f233b26
Mohan Nepali <nepalimohan@outlook.com>
2024-12-05 21:44:05 +0545

    settings.py comment revised

--- core/settings.py
+++ core/settings.py
@@ -124,12 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#registering custom user
+#registering custom user i.e. indicating user model as Customuser located at users/models.py
 AUTH_USER_MODEL = 'users.CustomUser'

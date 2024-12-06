9814ae52a24824017514786f8b8aae8e5d49e119
Sushovit204 <shahsushovit434@gmail.com>
2024-12-06 09:22:30 +0545

    Successfully setup fastapi

--- app/main.py
+++ app/main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
-# from .routers import phone_numbers
-# from .database import engine
-# from .models import Base
 
 app = FastAPI()
 
-Base.metadata.create_all(bind=engine)
-
-app.include_router(phone_numbers.router, prefix="/api/v1")
+@app.get("/")
+def read_root():
+    return{"message":"Hello World"}

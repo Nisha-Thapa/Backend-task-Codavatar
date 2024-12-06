a377db0b6277e21f35c2d32b8853dddff329c3fe
Sushovit204 <shahsushovit434@gmail.com>
2024-12-06 14:31:37 +0545

    refactor

--- app/main.py
+++ app/main.py
@@ -7,5 +7,8 @@ app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
+@app.get("/")
+async def root():
+    return {"message": "Please refer to /docs for API documentation"}
 
 app.include_router(phone_numbers.router, prefix="/api/v1")

d145633f6db9b7cda0d00af0e6cd10440b1d5cba
Kushal Dotel <kushal.dotel@study.lbef.edu.np>
2024-12-07 12:03:21 +0545

    feat: creating user login

--- cloud_telephony/telephony/serializers.py
+++ cloud_telephony/telephony/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
 
@@ -20,3 +22,30 @@ class RegisterSerializer(serializers.ModelSerializer):
             password=validated_data['password']
         )
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        """
+        Validate email and password, and return tokens upon success.
+        """
+        email = data.get('email')
+        password = data.get('password')
+
+        user = authenticate(email=email, password=password)
+        if not user:
+            raise serializers.ValidationError("Invalid email or password")
+
+        if not user.is_active:
+            raise serializers.ValidationError("This account is inactive")
+
+        # Generate JWT tokens
+        refresh = RefreshToken.for_user(user)
+        return {
+            'name':user.name,
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
--- cloud_telephony/telephony/views.py
+++ cloud_telephony/telephony/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer,LoginSerializer
 from rest_framework.permissions import AllowAny
 
 class RegisterView(APIView):
@@ -18,4 +18,16 @@ class RegisterView(APIView):
                 {"message": "User registered successfully."},
                 status=status.HTTP_201_CREATED
             )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    """
+    Handles user login by validating email and password.
+    Returns access and refresh tokens upon success.
+    """
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

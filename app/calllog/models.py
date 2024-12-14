779f4bc99e28050fc4e9846a64b79ca412480c98
_jATMOS <bks.mhryadayada@gmail.com>
2024-12-14 11:37:35 +0545

    chore: made models for virtualphoneno and calllog and registered to adminpage

--- app/calllog/models.py
+++ app/calllog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from users.models import CustomUser
+from virtualphoneno.models import VirtualPhoneNo
+
 
 # Create your models here.
+class CallLog(models.Model):
+    called_by = models.ForeignKey(
+        CustomUser, related_name="called_by", on_delete=models.CASCADE
+    )
+    called_no = models.ForeignKey(
+        VirtualPhoneNo,
+        related_name="called_no",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    received_no = models.ForeignKey(
+        VirtualPhoneNo,
+        related_name="received_no",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    called_at = models.DateTimeField(auto_now_add=True)
+    duration = models.DurationField()
+
+    class Meta:
+        verbose_name = "Call Log"
+        verbose_name_plural = "Call Logs"
+
+    def __str__(self):
+        return f"Call from {self.called_no.phone_no} to {self.received_no.phone_no} at {self.called_at}"
--- app/virtualphoneno/models.py
+++ app/virtualphoneno/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from users.models import CustomUser
+
 
 # Create your models here.
+class VirtualPhoneNo(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    phone_no = models.CharField(max_length=10, unique=True)
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="owner")
+
+    class Meta:
+        verbose_name = "Virtual Phoneno"
+        verbose_name_plural = "Virtual Phone Numbers"
+
+    def __str__(self):
+        return f"{self.phone_no} owned by {self.owner.email}"
